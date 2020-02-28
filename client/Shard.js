137a079edd04d55c83bc10724af5d7b7fded5992
DMVMarcio <playdznvlt@gmail.com>
2020-02-28 17:16:20 -0300

    provisory shardingManager

---
+++ client/Shard.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager(`./client/index.js`, { totalShards: 1 });
+
+manager.on('launch', shard => console.log(`Shard ${shard.id + 1}: iniciada com sucesso!`))
+
+manager.spawn();
